ba13e6e10566d099eafb67dfddeff6ee7735baf7
khnguyendinh <khnguyendinh@gmail.com>
2018-03-18 22:15:32 +0700

    hoc filter finished

--- app/learnAngularJs/filter/filter.js
+++ app/learnAngularJs/filter/filter.js
@@ -4,13 +4,14 @@ app.controller('filterCtrl',filterCtrl);
 
 function filterCtrl($scope) {
     var techonologis = [
-        {Name: "C#",Likes: 0,Dislikes: 3},
-        {Name: "java",Likes: 1,Dislikes: 2},
-        {Name: "python",Likes: 2,Dislikes: 1},
-        {Name: "Ruby",Likes: 3,Dislikes: 0}
+        {Name: "C#",Likes: 0,Dislikes: 300000000, BirthDay: new Date('12/3/2009'),status:true},
+        {Name: "java",Likes: 1,Dislikes: 2999999, BirthDay: new Date('12/4/2009'),status:false},
+        {Name: "python",Likes: 2,Dislikes: 1888888, BirthDay: new Date('12/5/2009'),status:true},
+        {Name: "Ruby",Likes: 3,Dislikes: 17777770, BirthDay: new Date('12/6/2009'),status:false}
     ];
     $scope.tech = techonologis;
     $scope.name = "techonologis";
+    $scope.limitRow = 3;
     $scope.increaLike = function (techo) {
         techo.Likes ++;
     };
@@ -18,3 +19,11 @@ function filterCtrl($scope) {
         techo.Likes --;
     };
 }
+app.filter("status",function (d) {
+    return function (input) {
+        if(input == true){
+            return "kich hoat";
+        }
+        return "khoa";
+    }
+});
